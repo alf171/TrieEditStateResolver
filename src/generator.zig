1ce47958884e49b2841176a73179906cbfbd6fcc
alf171 <alfonso.laffont@gmail.com>
2026-02-15 00:32:43 -0500

    make EditGenerator respect seed & default_init

--- src/generator.zig
+++ src/generator.zig
@@ -1,12 +1,13 @@
 const std = @import("std");
 const Edit = @import("edit.zig");
 const PathEdit = @import("pathEdit.zig");
+const Set = @import("set.zig");
 
 /// assume we are generating edits for a single document
 pub const EditGenerator = @This();
 
 /// reproduce randomness
-seed: u64,
+prng: std.Random.DefaultPrng,
 /// how many edits to generate per batch
 batch_size: u64,
 /// average pathEdits per edit
@@ -17,7 +18,20 @@ max_value_len: u64,
 /// what paths to apply edits across
 set_of_paths: std.StringArrayHashMap(void),
 
-pub fn next_batch(self: EditGenerator, alloc: std.mem.Allocator) ![]Edit {
+pub fn default_init(alloc: std.mem.Allocator) !EditGenerator {
+    const paths = try Set.of(&.{ "a/b", "a/c", "a/d", "a/e" }, alloc);
+    const seed = 0;
+    return EditGenerator{
+        .batch_size = 10,
+        .min_value_len = 10,
+        .max_value_len = 20,
+        .path_edits_per_edit = 2,
+        .prng = std.Random.DefaultPrng.init(seed),
+        .set_of_paths = paths.data,
+    };
+}
+
+pub fn next_batch(self: *EditGenerator, alloc: std.mem.Allocator) ![]Edit {
     const edits = try alloc.alloc(Edit, self.batch_size);
     for (0..self.batch_size) |batch_index| {
         const pathEdits = try alloc.alloc(PathEdit, self.path_edits_per_edit);
@@ -30,19 +44,21 @@ pub fn next_batch(self: EditGenerator, alloc: std.mem.Allocator) ![]Edit {
     return edits;
 }
 
-fn random_path(self: EditGenerator) []const u8 {
+fn random_path(self: *EditGenerator) []const u8 {
     const keys = self.set_of_paths.keys();
     std.debug.assert(keys.len > 0);
-    const idx = std.crypto.random.intRangeAtMost(usize, 0, keys.len - 1);
+    const random = self.prng.random();
+    const idx = random.intRangeAtMost(usize, 0, keys.len - 1);
     return keys[idx];
 }
 
-fn random_value(self: EditGenerator, alloc: std.mem.Allocator) ![]const u8 {
-    const length = std.crypto.random.intRangeAtMost(u64, self.min_value_len, self.max_value_len);
+fn random_value(self: *EditGenerator, alloc: std.mem.Allocator) ![]const u8 {
+    const random = self.prng.random();
+    const length = random.intRangeAtMost(u64, self.min_value_len, self.max_value_len);
     const buf = try alloc.alloc(u8, length);
     const alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
     for (buf) |*ch| {
-        const idx = std.crypto.random.intRangeAtMost(u8, 0, alphabet.len - 1);
+        const idx = random.intRangeAtMost(u8, 0, alphabet.len - 1);
         ch.* = alphabet[idx];
     }
     return buf;
